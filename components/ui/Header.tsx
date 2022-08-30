a55f46dda2c8bb6f3dcc1165d27e7ba993b866d7
Jeff Oliver <jeffoliver3384@gmail.com>
2022-08-30 02:12:30 -0400

    header component, fetching character data in get server side props

---
+++ components/ui/Header.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import React, { FC } from "react";
+import styles from "../../styles/Home.module.css";
+import logo from "../../public/assets/logo.png";
+
+const Header: FC = () => {
+  const { header, headerImg, center } = styles;
+
+  return (
+    <header className={`${header} ${center}`}>
+      <Image className={headerImg} src={logo} alt="Breaking Bad Logo" />
+    </header>
+  );
+};
+
+export default Header;
---
+++ types/typings.d.ts
@@ -0,0 +1,13 @@
+export interface Character {
+  char_id: number;
+  name: string;
+  nickname: string;
+  birthday: string;
+  img: string;
+  status: string;
+  category: string;
+  appearance: number[];
+  better_call_saul_appearance: number[];
+  occupation: string[];
+  portrayed: string[];
+}
